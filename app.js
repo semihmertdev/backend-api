d4d6c914c7e26560b081a8b6d8db1212a690ed32
semihmertdev <semihmert.dev@gmail.com>
2024-09-05 19:07:50 +0300

    update

--- app.js
+++ app.js
@@ -15,7 +15,4 @@ app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/comments', commentRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-module.exports = app;
+module.exports = app; // Export the app for Vercel's serverless function
